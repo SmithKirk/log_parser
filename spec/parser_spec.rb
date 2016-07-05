ffad19c2243eea7272178edfa1792f60fef596f6
Kirk Smith <kirks@hotmail.co.uk>
2016-07-05 13:35:17 +0100

    refactor spec- add context block

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -12,33 +12,40 @@ describe Parser do
     end
   end
 
-  describe '#parse_file' do
-    it 'will iterate over log file instiating a LoggedVisit in visits log' do
+  context 'after log has been parsed' do
+    before do
       parser.parse_file
-      expect(parser.visits_log).to include (LoggedVisit)
     end
-  end
 
-  describe '#visits_by_page' do
-    it 'groups visits by page' do
-      parser.parse_file
-      expect(parser.visits_by_page.size).to eq 4
+    describe '#parse_file' do
+      it 'will iterate over log file instiating a LoggedVisit in visits log' do
+        expect(parser.visits_log).to include (LoggedVisit)
+      end
     end
-  end
 
-  describe '#visits_by_page_counted_and_ordered' do
-    it 'will order pages by visit count' do
-      parser.parse_file
-      parser.visits_by_page_counted_and_ordered
-      expect(parser.visits_by_page_counted_and_ordered.size).to eq 4
+    describe '#visits_by_page' do
+      it 'groups visits by page' do
+        expect(parser.visits_by_page.size).to eq 4
+      end
     end
-  end
 
-  describe '#print_page_visits' do
-    it 'will display list of pages visited' do
-      parser.parse_file
-      expect(parser.print_page_visits).to eq ("/help_page/1 3 visits\n/home 2 visits\n/about 2 visits\n/contact 1 visits\n")
+    describe '#visits_by_page_counted_and_ordered' do
+      it 'will order pages by visit count' do
+        parser.visits_by_page_counted_and_ordered
+        expect(parser.visits_by_page_counted_and_ordered.size).to eq 4
+      end
     end
-  end
 
+    describe '#print_page_visits' do
+      it 'will display list of pages visited' do
+        expect(parser.print_page_visits).to eq ("/help_page/1 3 visits\n/home 2 visits\n/about 2 visits\n/contact 1 visits\n")
+      end
+    end
+
+    # describe '#unique_visits_counted_and_ordered' do
+    #   it 'will order pages by unique visits' do
+    #
+    #   end
+    # end
+  end
 end
